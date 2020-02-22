8ce280b982a34ff41063527941e4f22f1e51eb1e
lushaojie <1009636982@qq.com>
2020-02-22 20:41:34 +0800

    设置全局filter

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 设置全局过滤器
+Vue.filter('setWH', (url, arg) => {
+  // 图片路径尺寸自定义：https://lsj.com/w.h/img/1.jpg -> https://lsj.com/100.200/img/1.jpg
+  return url.replace(/w\.h/, arg);
+})
+
 Vue.config.productionTip = false
 
 new Vue({
